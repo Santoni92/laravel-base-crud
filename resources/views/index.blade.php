6e9007f3222a6a2a256a6228628a2e3afbc1bfdf
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-05-25 18:26:51 +0200

    creato layout ed usato yield per estendere il contenuto; aggiunto compilatore sass

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,16 @@
-@forelse ($comics as $comic)
-    <div>
-        <span>{{ $comic->title }}</span>
-        <p>{{ $comic->description }}</p>
-        <img src="{{ $comic->image }}" alt="">
-        <span>{{ $comic->price }}</span>
-        <span>{{ $comic->series }}</span>
-        <span>{{ $comic->sale_date }}</span>
-        <span>{{ $comic->type }}</span>
-    </div>
-@empty
-@endforelse
+@extends('layouts.app')
+@section('content')
+    @forelse ($comics as $comic)
+        <div class="container">
+            <span>{{ $comic->title }}</span>
+            <p>{{ $comic->description }}</p>
+            <img src="{{ $comic->image }}" alt="">
+            <span>{{ $comic->price }}</span>
+            <span>{{ $comic->series }}</span>
+            <span>{{ $comic->sale_date }}</span>
+            <span>{{ $comic->type }}</span>
+        </div>
+    @empty
+        non risultano esserci fumetti da visualizzare!!!
+    @endforelse
+@endsection
