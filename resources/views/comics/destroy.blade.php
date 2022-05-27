c6cd5eeb525161246cee140242b2160d1e768f35
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-05-27 17:11:18 +0200

    inserito form in 'show.blade' e passato $comic come parametro alla route()

--- resources/views/comics/destroy.blade.php
+++
@@ -1,5 +0,0 @@
-<form action="{{ route('comics.destroy', ['id' => $comic->id]) }}" method="POST">
-    @csrf
-    @method('DELETE')
-    <input type="submit" value="cancella">
-</form>
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,3 +3,9 @@
     {{ $comic->description }}
     <a href="{{ route('comics.index') }}">BACK</a>
 </div>
+
+<form action="{{ route('comics.destroy', $comic) }}" method="POST">
+    @csrf
+    @method('DELETE')
+    <input type="submit" value="cancella">
+</form>
